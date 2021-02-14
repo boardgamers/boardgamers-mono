650bfda08ec5bafcd8f059222e5e3840ccfa3b79
coyotte508 <coyotte508@gmail.com>
2021-02-14 12:11:45 +0100

    🚨 (admin-app) Fix prettier warnings

--- admin-app/src/components/GameEdit.vue
+++ admin-app/src/components/GameEdit.vue
@@ -10,7 +10,7 @@
         required
         multiple
         v-model="info.players"
-        :items="[2, 3, 4, 5, 6, 7, 8, 9, 10].map(x => ({ text: x + ' players', value: x }))"
+        :items="[2, 3, 4, 5, 6, 7, 8, 9, 10].map((x) => ({ text: x + ' players', value: x }))"
       />
 
       <div v-for="(viewer, i) in [info.viewer, info.viewer.alternate]" :key="i">
