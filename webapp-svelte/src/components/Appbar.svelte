22f40f493eeb711ca8cd9973d612618eecb541d0
coyotte508 <coyotte508@gmail.com>
2021-02-15 04:28:45 +0100

    🚧 (webapp-svelte) Touch up AppBar

--- webapp-svelte/src/components/Appbar.svelte
+++ webapp-svelte/src/components/Appbar.svelte
@@ -1,46 +1,81 @@
 
-<Navbar color="light" light expand="md">
-  <NavbarBrand href="/">sveltestrap</NavbarBrand>
-  <NavbarToggler on:click={() => (isOpen = !isOpen)} />
-  <Collapse {isOpen} navbar expand="md" on:update={handleUpdate}>
-    <Nav class="ml-auto" navbar>
+<Navbar color="light" light expand>
+  <!-- todo: reload game lists if on same page -->
+  <NavbarBrand href="/">BGS</NavbarBrand>
+  <!-- todo: mobile-only boardgame list -->
+
+  <Nav class="ml-auto" navbar>
+    {#if !user}
+      <!-- todo: hide on mobile -->
       <NavItem>
-        <NavLink href="#components/">Components</NavLink>
-      </NavItem>
-      <NavItem>
-        <NavLink href="https://github.com/bestguy/sveltestrap">GitHub</NavLink>
+        <span class="navbar-text">Have an account?</span>
       </NavItem>
       <UncontrolledDropdown nav inNavbar>
-        <DropdownToggle nav caret>Options</DropdownToggle>
-        <DropdownMenu right>
-          <DropdownItem>Option 1</DropdownItem>
-          <DropdownItem>Option 2</DropdownItem>
-          <DropdownItem divider />
-          <DropdownItem>Reset</DropdownItem>
+        <DropdownToggle nav caret>Login</DropdownToggle>
+        <DropdownMenu right class="login-dp">
+          <div class="row">
+            <div class="col-md-12">
+              Log in with
+              <div class="social-buttons">
+                <Button href="/api/account/auth/google" variant="google">Google</Button>
+                <Button href="/api/account/auth/discord" variant="discord">Discord</Button>
+                <Button href="/api/account/auth/facebook" variant="facebook">Facebook</Button>
+              </div>
+              or
+            </div>
+            <div class="bottom text-center">
+              New ? <a href="/signup"><b>Join us</b></a>
+            </div>
+          </div>
         </DropdownMenu>
       </UncontrolledDropdown>
-    </Nav>
-  </Collapse>
+    {:else}
+    {/if}
+  </Nav>
 </Navbar>
 
 <script lang="ts">
 import {
-  Collapse,
   Navbar,
-  NavbarToggler,
   NavbarBrand,
   Nav,
   NavItem,
-  NavLink,
   UncontrolledDropdown,
   DropdownToggle,
   DropdownMenu,
-  DropdownItem
+  Button,
+  Form,
+  FormGroup
 } from 'sveltestrap';
 
-let isOpen = false;
+let user = false;
 
-function handleUpdate(event: SvelteEvent<{isOpen: boolean}>) {
-  isOpen = event.detail.isOpen;
-}
 </script>
+
+<style type="text/scss" global>
+  .login-dp {
+    min-width: 250px;
+    padding: 14px 14px 0;
+    overflow: hidden;
+    background-color: rgba(255, 255, 255, 0.8);
+
+    .bottom {
+      border-top: 1px solid #ddd;
+      clear: both;
+      padding: 14px;
+    }
+
+    .social-buttons {
+      display: flex;
+      justify-content: space-around;
+      flex-wrap: wrap;
+      margin-top: 12px;
+      margin-bottom: 4px;
+
+      a {
+        width: 46%;
+        margin-bottom: 8px;
+      }
+    }
+  }
+</style>
