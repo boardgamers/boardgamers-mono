9cbe7365f7dcaaad7cccb1a29edb5c688d3e78c1
coyotte508 <coyotte508@gmail.com>
2021-03-12 19:20:13 +0100

    🎉 (webapp-svelte) Create vue-like router

---
+++ webapp-svelte/src/modules/router/RouterView.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+  import { route } from "./router";
+</script>
+
+{#if $route && $route.component}
+  <svelte:component this={$route.component} {...$route.params} />
+{:else}
+  <div><slot /></div>
+{/if}
