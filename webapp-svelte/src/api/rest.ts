39a181355b29d629de4dcfdb490af8d2c3441e07
coyotte508 <coyotte508@gmail.com>
2021-03-12 01:18:28 +0100

    ✨ (webapp-svelte) Persistent auth

--- webapp-svelte/src/api/rest.ts
+++ webapp-svelte/src/api/rest.ts
@@ -1,3 +1,7 @@
+import { get as $ } from "svelte/store";
+import type { Token } from "../store";
+import { accessToken, gamesAccessToken, refreshToken } from "../store";
+
 const baseUrl = "/api";
 
 function transformUrl(url: string) {
@@ -5,28 +9,63 @@ function transformUrl(url: string) {
 }
 
 export async function get(url: string) {
-  const response = await fetch(transformUrl(url));
-
-  if (response.status >= 400) {
-    throw await response.json();
-  }
-
-  if (response.headers.get("content-type")?.startsWith("text/plain")) {
-    return response.text();
-  }
-  return response.json();
+  const token = await getAccessToken(url);
+  return getResponseData(
+    await fetch(transformUrl(url), { headers: token ? { Authorization: `Bearer ${token.code}` } : {} })
+  );
 }
 
 export async function post(url: string, data: Record<string, unknown> = {}) {
-  const response = await fetch(transformUrl(url), {
-    method: "POST",
-    body: JSON.stringify(data),
-    headers: { "Content-Type": "application/json" },
-  });
+  const token = await getAccessToken(url);
+
+  return getResponseData(
+    await fetch(transformUrl(url), {
+      method: "POST",
+      body: JSON.stringify(data),
+      headers: { "Content-Type": "application/json", ...(token && { Authorization: `Bearer ${token.code}` }) },
+    })
+  );
+}
+
+async function getResponseData(response: Response) {
+  const body = response.headers.get("content-type")?.startsWith("application/json")
+    ? await response.json()
+    : await response.text();
 
   if (response.status >= 400) {
-    throw await response.json();
+    throw body;
   }
 
-  return await response.json();
+  return body;
+}
+
+/**
+ * Get accessToken, generating from refreshToken if needed, with the correct scopes
+ * @param url Used to determine the scopes needed for the accessToken
+ * @returns Access token or null if logged out
+ */
+async function getAccessToken(url: string): Promise<Token | null> {
+  if (!$(refreshToken)) {
+    return null;
+  }
+
+  const isGames = url.startsWith("/gameplay");
+
+  const ref = isGames ? gamesAccessToken : accessToken;
+  const $ref = $(ref);
+
+  if ($ref && $ref.expiresAt > Date.now() + 5 * 60 * 1000) {
+    return $ref;
+  }
+
+  const body = await getResponseData(
+    await fetch(transformUrl("/account/refresh"), {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ code: $(refreshToken)!.code, scopes: isGames ? ["gameplay"] : ["all"] }),
+    })
+  );
+
+  ref.set(body);
+  return body;
 }
