f3a9ece3c370957f8d5dbdfcb6f15fe8b2b428b6
coyotte508 <coyotte508@gmail.com>
2021-03-14 00:21:05 +0100

    🎨 (webapp-svelte) Refactor watcher generation

--- webapp-svelte/src/utils/watch.ts
+++ webapp-svelte/src/utils/watch.ts
@@ -3,26 +3,45 @@
  *
  * @param callback
  * @param deps
- * @param options You can give `{skipOnce: true}` to not run the first time. Must be stored in a variable and not directly in the body of the `watch` call
  *
  * @example
  *
  * ```
- * const options = {skipOnce: true}
  * const immediate = () => console.log("executed immediately and on changes")
- * const onlyOnChanges = () => console.log("executed only on changes")
  *
  * $ : watch(immediate, [a, b, c])
- * $ : watch(onlyOnChanges, [a, b, d], options)
  * ```
  */
-export function watch(callback: () => unknown, _deps: any[], options?: { skipOnce: boolean }) {
-  if (options?.skipOnce) {
-    options.skipOnce = false;
-    return;
-  }
-
+export function watch(callback: () => unknown, _deps: any[]) {
   callback();
+}
+
+/**
+ * Create a watcher. Compared to `watch`, it can have additional options
+ *
+ * @param callback The function to executre when the deps change
+ * @param options `options.immediate` is whether or not the function is executed
+ * immediately or only on changes
+ * @returns The watcher. Pass it the deps.
+ *
+ * @example
+ *
+ * ```
+ * const watcher = createWatcher(() => console.log("executed only on changes on a/b/c"), {immediate: false})
+ *
+ * $ : watcher(a, b, c)
+ * ```
+ */
+export function createWatcher(callback: () => unknown, options: { immediate: boolean }) {
+  let skip = !options.immediate;
+
+  return (..._deps: any[]) => {
+    if (skip) {
+      skip = false;
+      return;
+    }
+    callback();
+  };
 }
 
 export function skipOnce(callback: () => unknown) {
