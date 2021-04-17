fff3f0f195b3a92de0738f948ee17156b9a0987d
coyotte508 <coyotte508@gmail.com>
2021-04-17 12:12:35 +0200

    💩 (webapp-vue) Handle "select" preferences

--- webapp-vue/src/components/UserGameSettings.vue
+++ webapp-vue/src/components/UserGameSettings.vue
@@ -6,10 +6,25 @@
           <b-checkbox v-model="ownership" @change="postOwnership">I own this game</b-checkbox>
           <template v-if="game.preferences && game.preferences.length > 0">
             <hr />
-            <template v-for="pref in game.preferences">
-              <b-checkbox v-model="prefs.preferences[pref.name]" :key="pref.name" @change="postPreferences">{{
-                pref.label
-              }}</b-checkbox>
+            <template v-for="pref in prefItems">
+              <template v-if="pref.type === 'checkbox'">
+                <b-checkbox v-model="prefs.preferences[pref.name]" @change="postPreferences" :key="pref.name">
+                  {{ pref.label }}
+                </b-checkbox>
+              </template>
+              <template v-else-if="pref.type === 'select'">
+                <b-form-group :key="pref.name" :label="pref.label" label-cols="auto">
+                  <b-form-select
+                    v-model="prefs.preferences[pref.name]"
+                    @change="postPreferences"
+                    :options="pref.items.map(({ name, label }) => ({ value: name, text: label }))"
+                  >
+                  </b-form-select>
+                  <template #label>
+                    <span v-html="oneLineMarked(pref.label)" />
+                  </template>
+                </b-form-group>
+              </template>
             </template>
           </template>
         </template>
@@ -22,9 +37,7 @@
 import { Vue, Component, Prop, Watch } from "vue-property-decorator";
 import type { GameInfo } from "@lib/gameinfo";
 import marked from "marked";
-import { handleError } from "../utils";
-import { pick } from "lodash";
-import type { GamePreferences } from "@lib/gamepreferences";
+import { handleError, oneLineMarked } from "../utils";
 
 @Component
 export default class UserGameSettings extends Vue {
@@ -34,12 +47,22 @@ export default class UserGameSettings extends Vue {
   @Prop()
   game: GameInfo;
 
+  oneLineMarked = oneLineMarked;
+
   get prefs() {
     return this.$gameSettings.settings(this.game._id.game);
   }
 
   get prefItems() {
     let data = this.game.preferences;
+
+    if (this.prefs) {
+      for (const item of data) {
+        if (item.type === "select") {
+          this.prefs.preferences[item.name] = this.prefs.preferences[item.name] ?? item.items[0].name;
+        }
+      }
+    }
 
     if (data && this.game.viewer?.alternate?.url) {
       data = [{ name: "alternateUI", label: "Use alternate UI", type: "checkbox", items: null }, ...data];
