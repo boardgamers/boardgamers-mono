2d6a4d7d15ab5d2779249cd3aa761cb47a3eca56
coyotte508 <coyotte508@gmail.com>
2021-04-20 15:45:12 +0200

    fixup! 🚧 (webapp-svelte) Chatroom

--- api-node/app/models/chatmessage.ts
+++ api-node/app/models/chatmessage.ts
@@ -3,7 +3,9 @@ import makeSchema from "@lib/schemas/chatmessage";
 import { ObjectId } from "bson";
 import mongoose from "mongoose";
 
-export interface ChatMessageDocument extends mongoose.Document, ChatMessage<ObjectId> {}
+export interface ChatMessageDocument extends mongoose.Document, ChatMessage<ObjectId> {
+  _id: ObjectId;
+}
 
 const ChatMessage = mongoose.model("ChatMessage", makeSchema<ChatMessageDocument>());
 
--- game-server/app/models/chatmessage.ts
+++ game-server/app/models/chatmessage.ts
@@ -2,7 +2,9 @@ import { ChatMessage } from "@lib/chatmessage";
 import makeSchema from "@lib/schemas/chatmessage";
 import mongoose, { Types } from "mongoose";
 
-interface ChatMessageDocument extends mongoose.Document, ChatMessage<Types.ObjectId> {}
+interface ChatMessageDocument extends mongoose.Document, ChatMessage<Types.ObjectId> {
+  _id: Types.ObjectId;
+}
 
 const ChatMessage = mongoose.model("ChatMessage", makeSchema<ChatMessageDocument>());
 
