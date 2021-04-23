eba54be89988e79c8bc2ad75ab98b2f83b216d58
coyotte508 <coyotte508@gmail.com>
2021-04-23 21:09:35 +0200

    ✨ (webapp-svelte) Immediately add game to active games

--- webapp-svelte/src/store/game.ts
+++ webapp-svelte/src/store/game.ts
@@ -1,7 +1,18 @@
-import type { IGame } from "@lib/game";
-import { writable } from "svelte/store";
+import { get as $, writable } from "svelte/store";
 
-export const activeGames = writable<IGame[]>([]);
+export const activeGames = writable<string[]>([]);
 export const currentGameId = writable<string | null>(null);
 export const lastGameUpdate = writable<Date>(new Date(0));
 export const playerStatus = writable<Array<{ _id: string; status: "online" | "offline" | "away" }>>([]);
+
+export function addActiveGame(gameId: string) {
+  if (!$(activeGames).includes(gameId)) {
+    activeGames.update((games) => [...games, gameId]);
+  }
+}
+
+export function removeActiveGame(gameId: string) {
+  if ($(activeGames).includes(gameId)) {
+    activeGames.update((games) => games.filter((g) => g !== gameId));
+  }
+}
