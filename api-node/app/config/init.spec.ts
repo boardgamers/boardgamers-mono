7256b04173fe97bb38dfef119b4c47ada710ef2c
coyotte508 <coyotte508@gmail.com>
2021-06-27 23:11:55 +0200

    🔒️ (api-node) Prevent accidental DB wipe

--- api-node/app/config/init.spec.ts
+++ api-node/app/config/init.spec.ts
@@ -1,13 +1,20 @@
+import assert from "assert";
 import { Server } from "http";
 import mongoose from "mongoose";
 import { listen } from "../app";
 import initDb from "./db";
 import env from "./env";
 
+assert(process.env.NODE_ENV === "test");
+
 let server: Server;
 
 async function init() {
+  assert(env.database.bgs.name.endsWith("-test"));
   await initDb(env.database.bgs.url, false);
+
+  const users = await mongoose.connection.db.collection("users").countDocuments();
+  assert(users < 10, "This doesn't seem to be a test database");
   await mongoose.connection.db.dropDatabase();
 
   env.listen.port.api = 50606;
