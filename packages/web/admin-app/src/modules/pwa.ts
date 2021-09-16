04d1b764c68f07734ec7cd1256e969bff523731b
coyotte508 <coyotte508@gmail.com>
2021-09-16 22:56:59 +0200

    👷 (admin) Fix build

--- packages/web/admin-app/src/modules/pwa.ts
+++ packages/web/admin-app/src/modules/pwa.ts
@@ -1,12 +1,12 @@
-import { UserModule } from '~/types';
+// import { UserModule } from '~/types';
 
-// https://github.com/antfu/vite-plugin-pwa#automatic-reload-when-new-content-available
-export const install: UserModule = ({ isClient, router }) => {
-  if (!isClient)
-    return;
+// // https://github.com/antfu/vite-plugin-pwa#automatic-reload-when-new-content-available
+// export const install: UserModule = ({ isClient, router }) => {
+//   if (!isClient)
+//     return;
 
-  router.isReady().then(async() => {
-    const { registerSW } = await import('virtual:pwa-register');
-    registerSW({ immediate: true });
-  });
-};
+//   router.isReady().then(async() => {
+//     const { registerSW } = await import('virtual:pwa-register');
+//     registerSW({ immediate: true });
+//   });
+// };
