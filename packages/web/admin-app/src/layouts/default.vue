1cb3bfd06393170fb04373cf875fc784e5906dca
coyotte508 <coyotte508@gmail.com>
2021-09-20 00:30:01 +0200

    💄 (admin) Make UI a bit less ugly

--- packages/web/admin-app/src/layouts/default.vue
+++ packages/web/admin-app/src/layouts/default.vue
@@ -8,7 +8,7 @@ const isLoggedIn = computed(() => !!user.refreshToken);
 <template>
   <main class="text-gray-700 dark:text-gray-200 h-full" :class="{ isLoggedIn }">
     <nav-bar class="header" />
-    <navigation-drawer v-if="isLoggedIn" class="nav-drawer dark:bg-dark-700 bg-blue-400 overflow-y-auto" />
+    <navigation-drawer v-if="isLoggedIn" class="nav-drawer dark:bg-dark-700 bg-blue-300 border-r-indigo-600 border-r dark:border-r-dark-50 overflow-y-auto" />
     <router-view class="content mx-4 py-4" />
   </main>
 </template>
