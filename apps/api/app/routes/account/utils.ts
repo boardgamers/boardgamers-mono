035439c8bd94e06ac845c6f19c771761366e01ce
coyotte508 <coyotte508@gmail.com>
2021-12-20 20:45:11 +0100

    🔒️ (web) Fix login after refresh?

--- apps/api/app/routes/account/utils.ts
+++ apps/api/app/routes/account/utils.ts
@@ -8,12 +8,22 @@ export async function sendAuthInfo(ctx: Context) {
 
   await refreshToken.save();
 
+  const json = {
+    code: refreshToken.code,
+    expiresAt: refreshToken.createdAt.getTime() + 120 * 24 * 3600 * 1000,
+  };
+
+  // Should not be needed as already done in the front-end code, but sveltekit has weird issues
+  ctx.cookies.set("refreshToken", JSON.stringify(json), {
+    httpOnly: false,
+    expires: refreshToken.expiresAt,
+    secure: true,
+    sameSite: true,
+  });
+
   ctx.body = {
     user: ctx.state.user,
-    refreshToken: {
-      code: refreshToken.code,
-      expiresAt: refreshToken.createdAt.getTime() + 120 * 24 * 3600 * 1000,
-    },
+    refreshToken: json,
     accessToken: {
       code: await refreshToken.createAccessToken(["all"], ctx.state.user.isAdmin()),
       expiresAt: Date.now() + JwtRefreshToken.accessTokenDuration(),
