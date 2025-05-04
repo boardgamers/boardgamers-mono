fc38a427587ba59d4e357018f691846e4f5e5fc6
Gregor Zeitlinger <gregor.zeitlinger@grafana.com>
2025-05-04 14:38:39 +0200

    fix round

--- apps/web/src/components/Game/GameList.svelte
+++ apps/web/src/components/Game/GameList.svelte
@@ -119,7 +119,7 @@
             <div class="me-auto" style="line-height: 1.1">
               <div>
                 {#if game.status === "active"}
-                  <Badge color="contrast" class="small text-light">R{game.context.round}</Badge>
+                  <Badge color="contrast" class="small text-light">R{game.context?.round ?? 0}</Badge>
                 {/if}
                 <span class="game-name">
                   {game._id}
