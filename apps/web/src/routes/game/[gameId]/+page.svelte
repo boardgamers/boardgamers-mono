a2c9095b99dbc24f382394dfad549bff5a9b3ec8
coyotte508 <coyotte508@protonmail.com>
2026-07-31 11:51:52 +0000

    Add YourTurnBanner component to game page

--- apps/web/src/routes/game/[gameId]/+page.svelte
+++ apps/web/src/routes/game/[gameId]/+page.svelte
@@ -1,11 +1,13 @@
 <script lang="ts">
-  import { OpenGame, StartedGame, ChatRoom } from "@/components";
+  import { OpenGame, StartedGame, ChatRoom, YourTurnBanner } from "@/components";
   import type { GameFront, PlayerInfoFront, GameInfoFront } from "@bgs/models";
   import { page } from "$app/state";
 
   let game = $derived((page.data.game as GameFront)!);
   let gameId = $derived(game._id);
 </script>
+
+<YourTurnBanner />
 
 {#if game.status === "open"}
   <OpenGame />
