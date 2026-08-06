ef8a31d208625d9510bbeecca913c8a34ec129d9
coyotte508 <coyotte508@protonmail.com>
2026-08-06 21:00:11 +0000

    Remove pulse animation from appbar game indicator

--- apps/web/src/components/Game/index.ts
+++ apps/web/src/components/Game/index.ts
@@ -4,4 +4,3 @@ export { default as GameSidebar } from "./GameSidebar.svelte";
 export { default as OpenGame } from "./OpenGame.svelte";
 export { default as PlayerGameAvatar } from "./PlayerGameAvatar.svelte";
 export { default as StartedGame } from "./StartedGame.svelte";
-
